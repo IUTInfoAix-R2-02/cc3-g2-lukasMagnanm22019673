19527af4538ecfc643d8d0b43d08d2cb08d08ede
m22019673 <lukas.magnan@etu.univ-amu.fr>
2023-06-02 16:27:43 +0200

    Partie 2

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -24,26 +24,85 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class ToileController implements Initializable {
 
-    private static int rayonCercleExterieur = 200;
-    private static int angleEnDegre = 60;
-    private static int angleDepart = 90;
-    private static int noteMaximale = 20;
+public class ToileController {
+
+    @FXML
+    private TextField comp1;
+
+    @FXML
+    private TextField comp2;
+
+    @FXML
+    private TextField comp3;
+
+    @FXML
+    private TextField comp4;
+
+    @FXML
+    private TextField comp5;
+
+    @FXML
+    private TextField comp6;
+
+    @FXML
+    private void tracerButtonClicked() {
+
+        double note1 = Double.parseDouble(comp1.getText());
+        double note2 = Double.parseDouble(comp2.getText());
+        double note3 = Double.parseDouble(comp3.getText());
+        double note4 = Double.parseDouble(comp4.getText());
+        double note5 = Double.parseDouble(comp5.getText());
+        double note6 = Double.parseDouble(comp6.getText());
 
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+        double x1 = getXRadarChart(note1, 1);
+        double y1 = getYRadarChart(note1, 1);
+
+        double x2 = getXRadarChart(note2, 2);
+        double y2 = getYRadarChart(note2, 2);
+
+        double x3 = getXRadarChart(note3, 3);
+        double y3 = getYRadarChart(note3, 3);
+
+        double x4 = getXRadarChart(note4, 4);
+        double y4 = getYRadarChart(note4, 4);
+
+        double x5 = getXRadarChart(note5, 5);
+        double y5 = getYRadarChart(note5, 5);
+
+        double x6 = getXRadarChart(note6, 6);
+        double y6 = getYRadarChart(note6, 6);
+
     }
 
-    int getXRadarChart(double value, int axe ){
-        return (int) (rayonCercleExterieur + Math.cos(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
-                *  (value / noteMaximale));
+    @FXML
+    private void viderButtonClicked() {
+
+        comp1.clear();
+        comp2.clear();
+        comp3.clear();
+        comp4.clear();
+        comp5.clear();
+        comp6.clear();
     }
 
-    int getYRadarChart(double value, int axe ){
-        return (int) (rayonCercleExterieur - Math.sin(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
-                *  (value / noteMaximale));
+    @FXML
+    private void competenceTextFieldAction() {
+        tracerButtonClicked();
     }
 
+    private double getXRadarChart(double value, int axe) {
+        double maxNote = 20.0;
+        double angle = (2 * Math.PI / 6) * axe;
+        double radius = (value / maxNote) * 20.0;
+        return Math.cos(angle) * radius;
+    }
+
+    private double getYRadarChart(double value, int axe) {
+        double maxNote = 20.0;
+        double angle = (2 * Math.PI / 6) * axe;
+        double radius = (value / maxNote) * 20.0;
+        return Math.sin(angle) * radius;
+    }
 }
